20e4f94999dccf1b050d6d0293969e1a6a954ed0
Alper Kaya <kayaalper@sabanciuniv.edu>
2020-07-06 17:30:56 +0900

    testing fix for heroku #3

--- html-upload/app/main.py
+++ html-upload/app/main.py
@@ -9,24 +9,31 @@ def hello():
     if recv_file is None:
         return "", 400
     try:
-        recv_file.save(f"./images/{recv_file.filename}")
-    except Exception as e:
-        os.mkdir("./images/")
-        recv_file.save(f"./images/{recv_file.filename}")
+        recv_file.save(f"app/images/{recv_file.filename}")
+    except:
+        os.mkdir("./app/images/")
+        recv_file.save(f"app/images/{recv_file.filename}")
     return "OK", 200
 
 
 @app.route("/<filename>")
 def main(filename):
-    return send_file(f"./static/{filename}")
+    try:
+        return send_file(f"static/{filename}")
+    except:
+        return "", 404
+    
 
 
 @app.route("/image/<filename>")
 def serve_image(filename):
-    return send_file(f"./images/{filename}")
+    try:
+        return send_file(f"images/{filename}")
+    except Exception as e:
+        return "",404
 
 
 @app.route("/")
 def root_directory():
-    return send_file("./static/main.html")
+    return send_file("static/main.html")
 
--- html-upload/wsgi.py
+++ html-upload/wsgi.py
@@ -2,5 +2,5 @@
 from app.main import app 
   
 if __name__ == "__main__": 
-        app.run(debug=True) 
+        app.run() 
 
